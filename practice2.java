fe2591401fc0665883131032d6d3226749eb9ccb
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-10-21 04:01:21 +0000

    Stringクラス　例

---
+++ practice2.java
@@ -0,0 +1,19 @@
+public class StringTest{
+  public static void main(String... args){
+    if (args.length < 1){
+      System.err.println("引数にメールのFromヘッダを指定してください");
+      // プログラムを途中で終了する簡易コード
+      System.exit(-1);
+    }
+    String input = args[0];
+    int begin = input.indexOf('<');
+    if (begin >= 0){
+      begin ++;
+      int end = input.indexOf('>');
+      if (end > begin){
+        String addr = input.substring(begin, end);
+        System.out.println(addr);
+      }
+    }
+  }
+}
