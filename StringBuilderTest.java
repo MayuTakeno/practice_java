e950cf8c6ed4d12708df66598cc322cde8362440
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-10-23 08:21:26 +0000

    java文字出力成功

--- StringBuilderTest.java
+++ StringBuilderTest.java
@@ -3,19 +3,20 @@ public class StringBuilderTest{
   static void stringBuilderToUpperCase(StringBuilder sb){
     for(int i = 0; i < sb.length(); i ++){
       char c = sb.charAt(i);
-      char uc = Character.ToUpperCase(c);
+      char uc = Character.toUpperCase(c);
       sb.setCharAt(i, uc);
     }
   }
   public static void main(String... args){
-    if( args.length < 1){
+    if(args.length < 1){
       System.err.println("引数に文字列を指定してください");
+      // システム終了コマンド
       System.exit(-1);
     }
     StringBuilder input = new StringBuilder(args[0]);
     // もし第一引数の文字列が"abc"であれば
     stringBuilderToUpperCase(input);
     // この行が"ABC"を出力する
-    System.println(input);
+    System.out.println(input);
   }
 }
