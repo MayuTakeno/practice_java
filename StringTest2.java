45654d9f41637112858b650ff18bf1a8752191b7
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-10-25 07:39:30 +0000

    Java復習

--- StringTest2.java
+++ StringTest2.java
@@ -1,18 +1,17 @@
-public class StringTest{
+public class StringTest2{
   public static void main(String... args){
-    if (args.length < 1){
+    if(args.length < 1){
+      // エラー分を出力
       System.err.println("引数にメールのFromヘッダを指定してください");
-      // プログラムを途中で終了する簡易コード
+      // 処理終了
       System.exit(-1);
     }
-    // コマンドライン引数の一つ目を表示
     String input = args[0];
-    // 文字列から"<"と">"が両方見つかった場合,substringでその間の文字列を出力する
     int begin = input.indexOf('<');
-    if (begin >= 0){
+    if(begin >= 0){
       begin ++;
       int end = input.indexOf('>');
-      if (end > begin){
+      if(end > begin){
         String addr = input.substring(begin, end);
         System.out.println(addr);
       }
