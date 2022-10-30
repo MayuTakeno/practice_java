9e229820d7ea2ab4de281adc44fc11d8ef383623
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-10-30 16:54:09 +0000

    Java変数代入

--- Example1.java
+++ Example1.java
@@ -1,9 +1,12 @@
 public class Example1{
   public static void main(String... args){
     int i;
+    int j;
     i = 5;
     System.out.println(i);
     i = 10;
     System.out.println(i);
+    j = i;
+    System.out.println(j);
   }
 }
