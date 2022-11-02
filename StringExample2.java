356ae2f5f494263069f5527ab89c27ffa07aa958
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-11-02 10:50:34 +0000

    java 文字列の連結

---
+++ StringExample2.java
@@ -0,0 +1,8 @@
+public class StringExample2{
+  public static void main(String... args){
+    String message1 = "こんにちは";
+    String message2 = "ケティ";
+    String message3 = message1 + message2;
+    System.out.println(message3);
+  }
+}
