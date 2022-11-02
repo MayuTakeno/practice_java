6a761819633ffc5b468f4d4dbb33d570a0299e50
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-11-02 11:28:43 +0000

    java if~else

---
+++ Examples2/IfExample2.java
@@ -0,0 +1,10 @@
+public class IfExample2{
+  public static void main(String... args){
+    int age = 20;
+    if(age >= 25){
+      System.out.println("25歳以上ですね");
+    }else{
+    System.out.println("25歳以下ですね");
+    }
+  }
+}
