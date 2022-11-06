435dbfe50a891cb010d2f2e95123b76cb2e9f75b
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-11-06 08:33:20 +0000

    java 繰り返し処理

---
+++ Examples2/ForExample3.java
@@ -0,0 +1,8 @@
+public class ForExample3{
+  public static void main(String... args){
+    for(int i = 0; i < 10; i++){
+      System.out.println(i);
+    }
+    System.out.println("処理を終了します");
+  }
+}
