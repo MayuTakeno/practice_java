57e0306fb5f4aa47e48a675deb57622e8d7837d4
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-11-13 03:37:15 +0000

    java 偶数出力

---
+++ Examples3/ContinueExample3.java
@@ -0,0 +1,10 @@
+public class ContinueExample3{
+  public static void main(String... args){
+    for(int i = 1; i <= 15; i++){
+      if(i % 2 == 1){
+        continue;
+      }
+      System.out.println("変数iは" + i);
+    }
+  }
+}
