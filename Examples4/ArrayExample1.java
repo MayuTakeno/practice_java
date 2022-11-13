d20a872ad5665e1d0e3c1d5e97c591dd4b872a8e
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-11-13 03:54:46 +0000

    java 1次元配列

---
+++ Examples4/ArrayExample1.java
@@ -0,0 +1,8 @@
+public class ArrayExample1{
+  public static void main(String... args){
+    int[] scores = {50, 55, 60, 65, 70};
+    for(int i = 0; i < 5; i ++){
+      System.out.println(scores[i]);
+    }
+  }
+}
