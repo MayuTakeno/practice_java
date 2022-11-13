92ffeb5be131db8642502b03f78e1f2b14f36275
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-11-13 03:42:43 +0000

    java ループ処理のネスト

---
+++ Examples3/NestExample1.java
@@ -0,0 +1,10 @@
+public class NestExample1{
+  public static void main(String... args){
+    for(int a = 1; a <= 3; a++){
+      System.out.println("A=" + a);
+      for(int b = 1; b <=3; b++){
+        System.out.println("B=" + b);
+      }
+    }
+  }
+}
